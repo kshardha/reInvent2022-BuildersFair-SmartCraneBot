9a6a09c46ff59115561fe39de243e715744cb2dd
kshardha <kshardha@gmail.com>
2022-10-10 16:59:48 -0400

    Added session-id and storing it in DDB

--- Lambda Functions/SentimentAnalysis.py
+++ Lambda Functions/SentimentAnalysis.py
@@ -1,5 +1,9 @@
 import json
 import boto3
+import random, string
+
+leaderboard_ddb_table = "LeaderBoard"
+
 
 def lambda_handler(event, context):
     
@@ -18,10 +22,24 @@ def lambda_handler(event, context):
     sentiment = response.get("Sentiment")
     #print(sentiment)
     
-    if(sentiment == "POSITIVE" or sentiment == "NEUTRAL" or sentiment == "MIXED"):
+    if(sentiment == "POSITIVE" or sentiment == "NEUTRAL"):
         print(sentiment)
         response_body = "Sentiment analysis has completed. It is " + sentiment
-    elif(sentiment == "NEGATIVE"):
+        
+        # Generate Session ID and store it in the table
+        
+        random_session_id = ''.join(random.choices(string.ascii_letters + string.digits, k=25))
+        print(random_session_id)
+        dynamodb_client = boto3.client('dynamodb')
+        dynamodb_client.put_item(TableName=leaderboard_ddb_table, Item={'session-id':{'S':random_session_id}, 'msg':{'S': event_payload}})
+        
+        # Call Timer functionality
+        
+        # Enable IoT Things
+        
+        # Call ML Inference piece
+        
+    elif(sentiment == "NEGATIVE" or sentiment == "MIXED"):
         print("Sentiment is negative")
         response_body = "Sentiment analysis has completed. It is " + sentiment
     
