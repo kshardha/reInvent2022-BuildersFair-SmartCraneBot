7c69a74e8d65102b80fdef2bd99525fd3f82c631
kshardha <kshardha@gmail.com>
2022-10-27 13:37:34 -0400

    changes to the sentiment analysis lambda code - backing API GW

--- Lambda Functions/SentimentAnalysis.py
+++ Lambda Functions/SentimentAnalysis.py
@@ -2,18 +2,21 @@ import json
 import boto3
 import random, string
 
-leaderboard_ddb_table = "LeaderBoard"
-
+random_session_id = ""
+game_duration_value = ""
+sentiment = ""
+step_function_arn = ""
 
 def lambda_handler(event, context):
     
     # Get API body containing the message
-    event_payload = event.get("body")
-    #print(event_payload)
+    print(event)
+    event_payload = event.get("msg")
+    print(event_payload)
     
     # Calling Comprehend Detect Sentiment API
-    client = boto3.client('comprehend')
-    response = client.detect_sentiment(
+    comprehend_client = boto3.client('comprehend')
+    response = comprehend_client.detect_sentiment(
         Text=event_payload,
         LanguageCode='en'
     )
@@ -26,25 +29,55 @@ def lambda_handler(event, context):
         print(sentiment)
         response_body = "Sentiment analysis has completed. It is " + sentiment
         
-        # Generate Session ID and store it in the table
+        ps_client = boto3.client('ssm')
         
+        # Get DDB table name from Parameter Store
+        ps_parameter = ps_client.get_parameter(
+            Name='LeaderBoard_DDB_Table_Name'
+        )
+        
+        leaderboard_ddb_table = str(ps_parameter.get("Parameter").get("Value"))
+        print("DDB Table Name: " + leaderboard_ddb_table)
+        
+        # Generate Session ID and store it in the table
         random_session_id = ''.join(random.choices(string.ascii_letters + string.digits, k=25))
         print(random_session_id)
         dynamodb_client = boto3.client('dynamodb')
         dynamodb_client.put_item(TableName=leaderboard_ddb_table, Item={'session-id':{'S':random_session_id}, 'msg':{'S': event_payload}})
         
-        # Call Timer functionality
+        # Get GameDuration value from Parameter Store
+        game_duration_parameter = ps_client.get_parameter(
+            Name='GameDuration'
+        )
+        game_duration_value = str(game_duration_parameter.get("Parameter").get("Value"))
+        print("Game Duration: " + game_duration_value)
         
-        # Enable IoT Things
+        # Call Steps for Locking and Unlocking of Arm
         
-        # Call ML Inference piece
+        step_function_parameter = ps_client.get_parameter(
+            Name='Step_Function_Arn'
+        )
+        step_function_parameter_value = str(step_function_parameter.get("Parameter").get("Value"))
+        print("Step Function arn: " + step_function_parameter_value)
+        
+        step_function_client = boto3.client('stepfunctions')
+        response = step_function_client.start_execution(
+            stateMachineArn=step_function_parameter_value
+        )
+        print(response)
         
     elif(sentiment == "NEGATIVE" or sentiment == "MIXED"):
         print("Sentiment is negative")
         response_body = "Sentiment analysis has completed. It is " + sentiment
+        random_session_id = ""
+        game_duration_value = ""
     
     
     return {
         'statusCode': 200,
-        'body': json.dumps(response_body)
+        'body': response_body,
+        'sentiment': sentiment,
+        'game_duration': game_duration_value,
+        'session_id': random_session_id
+        
     }
