9aff4ca8803dbe44020c2f6f57f0905d492148cd
Narcisse Z <nzekpa@gmail.com>
2022-11-22 00:25:17 -0500

    Update display_manager.py

--- celebrartion/display_manager.py
+++ celebrartion/display_manager.py
@@ -3,18 +3,16 @@ from subprocess import Popen, TimeoutExpired
 import time
 
 DISPLAY_DELAY = 10
-DEFAULT_DISPLAY_IMAGE="./test.ppm"
-CELEBRATION_DISPLAY_IMAGE=""
-CMD = ["./demo", "--led-rows=32", "--led-cols=64", "-D1"]
+GOAL_CMD = ["./demo", "--led-rows=32", "--led-cols=64", "-D1", "./goal.ppm"]
 
 class TextScroller:
-    def __init__(self, text_ppm_file):
+    def __init__(self,command, text_ppm_file):
         self.text_ppm_file = text_ppm_file
-        self.test_text = "testing"
+        self.command = command
 
     def start_scroll_text(self, timeout=-1):
         try:
-            self.process = Popen(CMD, shell=True)
+            self.process = Popen(self.command)
             if timeout>0:
                 self.process.wait(timeout)
         except TimeoutExpired as exp:
@@ -24,7 +22,7 @@ class TextScroller:
 
     def stop_scroll_text(self):
         try:
-            if self.process is not None: 
+            if self.process is not None:
                 self.process.kill()
         except Exception as ex:
             print(ex)
@@ -33,11 +31,11 @@ class TextScroller:
         return self.process
 
     def run(self):
-        th = Thread(target=self.start_scroll_text, args=[])
+        th = Thread(target=self.start_scroll_text, args=[30])
         th.start()
 
 
 
 if __name__ == '__main__':
-    t = TextScroller(DEFAULT_DISPLAY_IMAGE, 30)
+    t = TextScroller(GOAL_CMD, "goal")
     t.run()
