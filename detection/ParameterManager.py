45ca18ac472b2f8c01263ce66287023dfe449c8c
djiten75 <57147167+djiten75@users.noreply.github.com>
2022-12-02 14:52:30 -0500

    Update ParameterManager.py

--- detection/ParameterManager.py
+++ detection/ParameterManager.py
@@ -1,25 +1,34 @@
 import boto3
 import json
 
-
 GOAL_BOUNDING_BOX='goal_bounding_box'
 TIER1_BOUNDING_BOX='tier1_bounding_box'
 TIER2_BOUNDING_BOX='tier2_bounding_box'
 IMAGE_DIMENSIONS='image_dimensions'
+SHOT_START_Y_THRESHOLD='shot_start_y_threshold'
 
 local_cache = {}
 
 ssm = boto3.client('ssm')
 
+def set_ssm(ssm_client):
+    global ssm
+    ssm = ssm_client
+
 def get_shot_tier_bounding_box(tier):
-    if 'tier_'+tier not in local_cache:    
+    local_cache={}
+    if 'tier_'+tier not in local_cache:
         if tier == '1':
             local_cache['tier_'+tier] = get_json_param(TIER1_BOUNDING_BOX)
+            print("tier 1:", local_cache['tier_1'])
         elif tier == '2':
             local_cache['tier_'+tier] = get_json_param(TIER2_BOUNDING_BOX)
+            print("tier 2:", local_cache['tier_2'])
         elif tier == 'GOAL':
             local_cache['tier_'+tier] = get_json_param(GOAL_BOUNDING_BOX)
+            print("tier GOAL:", local_cache['tier_GOAL'])
     return local_cache['tier_'+tier]
+    
     
 
 def get_image_dimensions():
@@ -27,9 +36,18 @@ def get_image_dimensions():
         local_cache['image_dimensions'] = get_json_param(IMAGE_DIMENSIONS)
     return local_cache['image_dimensions']
 
+def get_shot_start_y_threshold():
+    if 'shot_start_y_threshold' not in local_cache:
+        local_cache['shot_start_y_threshold'] = get_string_param(SHOT_START_Y_THRESHOLD)
+    return local_cache['shot_start_y_threshold']
+
 def get_json_param(param_name):
     parameter = ssm.get_parameter(Name=param_name)
     return json.loads(str(parameter['Parameter']['Value']))
+
+def get_string_param(param_name):
+    parameter = ssm.get_parameter(Name=param_name)
+    return str(parameter['Parameter']['Value'])
 
 #def get_goal_bounding_box():
 #    return {'x1': 150, 'y1': 9, 'x2': 200, 'y2': 36}
